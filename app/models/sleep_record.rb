2eb9c7a449c763c19e6daecf08bf9ad4df06872d
Yoshi-55 <ysmr9855@gmail.com>
2025-09-10 11:43:24 +0900

    ✨ feat: #22 睡眠記録レコード作成 bed_time, wake_time, noteを追加 migrateまで完了

---
+++ app/models/sleep_record.rb
@@ -0,0 +1,18 @@
+class SleepRecord < ApplicationRecord
+  belongs_to :user
+
+  validates :bed_time, presence: true
+  validates :wake_time, presence: true
+  validate :wake_time_after_bed_time
+
+  validates :note, length: { maximum: 100 }, allow_blank: true
+
+  private
+
+  def wake_time_after_bed_time
+    return if bed_time.blank? || wake_time.blank?
+    if wake_time <= bed_time
+      errors.add(:wake_time, "無効な入力です。起床時間は就寝時間より後に設定してください。")
+    end
+  end
+end
