74f841e469ec4afc51f6f9954a1d534da91f1ae0
Yoshi-55 <ysmr9855@gmail.com>
2025-09-10 23:08:16 +0900

    ✨ feat: UserとSleepRecordの1:N関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :sleep_records, dependent: :destroy
 end
