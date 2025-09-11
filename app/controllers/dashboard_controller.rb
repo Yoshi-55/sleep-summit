5e006bff03a6720cbe0a25f595137dab9cb13a30
Yoshi-55 <ysmr9855@gmail.com>
2025-09-11 12:47:03 +0900

    ✨ feat: 睡眠、起床記録機能　追加

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,4 +4,23 @@ class DashboardController < ApplicationController
   def index
     @sleep_records = current_user.sleep_records.order(bed_time: :desc)
   end
+
+  def start_sleep
+    if current_user.sleep_records.where(wake_time: nil).exists?
+      redirect_to authenticated_root_path, alert: "すでに就寝記録があります"
+    else
+      current_user.sleep_records.create!(bed_time: Time.current)
+      redirect_to authenticated_root_path, notice: "就寝時刻を記録しました"
+    end
+  end
+
+  def wake_up
+    record = current_user.sleep_records.where(wake_time: nil).last
+    if record
+      record.update!(wake_time: Time.current)
+      redirect_to authenticated_root_path, notice: "起床時刻を記録しました"
+    else
+      redirect_to authenticated_root_path, alert: "睡眠時刻の記録なし、起床時刻を記録できません"
+    end
+  end
 end
