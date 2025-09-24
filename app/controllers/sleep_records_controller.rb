139f45225804263b48aa57abe9591b24f377d227
Yoshi-55 <ysmr9855@gmail.com>
2025-09-24 11:22:26 +0900

    ♻️ refactor: DashboardとHistoryを分離し、SleepRecordのビューと統計をリファクタ

--- app/controllers/sleep_records_controller.rb
+++ app/controllers/sleep_records_controller.rb
@@ -1,20 +1,6 @@
 class SleepRecordsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_unwoken_record, only: [ :update ]
-
-  def index
-    @sleep_records = current_user.sleep_records.order(:bed_time)
-    @unwoken_record = current_user.sleep_records.unwoken.first
-
-    @series = SleepRecord.build_series(@sleep_records, days: 7)
-
-    @weekly_records = SleepRecord.build_weekly_cumulative(@sleep_records, days: 7)
-
-    finished_records = @sleep_records.select(&:wake_time)
-    @today_sleep = finished_records.find { |r| r.bed_time >= Time.current.beginning_of_day && r.wake_time <= Time.current.end_of_day }
-    @total_sleep_hours = SleepRecord.total_sleep_hours(finished_records)
-    @average_sleep_hours = finished_records.any? ? (@total_sleep_hours / finished_records.size).round(2) : 0
-  end
+  before_action :set_unwoken_record, only: [:update]
 
   def create
     if @unwoken_record
