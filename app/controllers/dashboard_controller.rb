5e994776c272e333a8f050331c03a353b6f479ad
Yoshi-55 <ysmr9855@gmail.com>
2025-09-26 16:00:14 +0900

    ✨ feat: 記録表示を週、月単位に変更 dashboard：集表示 history：月表示

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,14 +3,15 @@ class DashboardController < ApplicationController
 
   def index
     sleep_records = current_user.sleep_records.order(:bed_time)
-    @unwoken_record = current_user.sleep_records.unwoken.first
+    @unwoken_record = sleep_records.unwoken.first
 
     @series = SleepRecord.build_series(sleep_records, days: 7)
-
     @weekly_records = SleepRecord.build_weekly_cumulative(sleep_records, days: 7)
 
     finished_records = sleep_records.select(&:wake_time)
-    @today_sleep = finished_records.find { |r| r.bed_time >= Time.current.beginning_of_day && r.wake_time <= Time.current.end_of_day }
+    @today_sleep = finished_records.find do |r|
+      r.bed_time.to_date == Date.current && r.wake_time.to_date == Date.current
+    end
     @total_sleep_hours = SleepRecord.total_sleep_hours(finished_records)
     @average_sleep_hours = finished_records.any? ? (@total_sleep_hours / finished_records.size).round(2) : 0
   end
