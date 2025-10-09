6bc68eecbf6957d6dffd88e033c99c1107a1b667
Yoshi-55 <ysmr9855@gmail.com>
2025-10-09 12:44:42 +0900

    ✨ feat: 日別データから累積睡眠・起床時間を計算

--- app/models/sleep_record.rb
+++ app/models/sleep_record.rb
@@ -77,10 +77,6 @@ class SleepRecord < ApplicationRecord
 
   private
 
-  def wake_time_after_bed_time
-    return if bed_time.blank? || wake_time.blank?
-    if wake_time <= bed_time
-      errors.add(:wake_time, "無効な入力です。起床時間は就寝時間より後に設定してください。")
-    end
+
   end
 end
