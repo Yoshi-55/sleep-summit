4cf22639f812a36663208f7c4139f5c5a6669f20
Yoshi-55 <ysmr9855@gmail.com>
2025-10-09 16:33:21 +0900

    ✨ feat: ユーザーごとの起床、就寝時刻の管理 承認ユーザーのみアクセス可能
    未就寝レコードがある時は新規記録を防ぐ
    フロントでも押せないようにするがこちらでもエラーメッセージ表示（今後の編集昨日にも対応するため）

--- app/controllers/sleep_records_controller.rb
+++ app/controllers/sleep_records_controller.rb
@@ -1,29 +1,43 @@
 class SleepRecordsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_unwoken_record, only: [ :update ]
+  before_action :set_unwoken_record
 
   def create
-    if @unwoken_record
-      redirect_with_flash(:alert, "すでに未起床レコードがあります") and return
-    end
-
-    current_user.sleep_records.create!(bed_time: Time.current)
-    redirect_with_flash(:notice, "就寝時刻を記録しました")
+    return redirect_with_flash(:alert, "すでに未就寝レコードがあります") if @unwoken_record
+    create_wake_record
   end
 
   def update
-    if @unwoken_record
-      @unwoken_record.update!(wake_time: Time.current)
-      redirect_with_flash(:notice, "起床時刻を記録しました")
-    else
-      redirect_with_flash(:alert, "未起床レコードがありません")
-    end
+    return redirect_with_flash(:alert, "未就寝レコードがありません") unless @unwoken_record
+    update_bed_time
   end
 
   private
 
   def set_unwoken_record
-    @unwoken_record = current_user.sleep_records.unwoken.first
+    @unwoken_record = current_user.sleep_records.unbedded.first
+  end
+
+  def create_wake_record
+    last_record = current_user.sleep_records.order(:wake_time).last
+    if last_record && last_record.bed_time.nil?
+      return redirect_with_flash(:alert, "前回の就寝時刻を先に記録してください")
+    end
+
+    sleep_record = current_user.sleep_records.build(wake_time: Time.current)
+    if sleep_record.save
+      redirect_with_flash(:notice, "起床時刻を記録しました")
+    else
+      redirect_with_flash(:alert, "起床時刻の記録に失敗しました: #{sleep_record.errors.full_messages.join(', ')}")
+    end
+  end
+
+  def update_bed_time
+    if @unwoken_record.update(bed_time: Time.current)
+      redirect_with_flash(:notice, "就寝時刻を記録しました")
+    else
+      redirect_with_flash(:alert, "就寝時刻の記録に失敗しました: #{@unwoken_record.errors.full_messages.join(', ')}")
+    end
   end
 
   def redirect_with_flash(type, message)
