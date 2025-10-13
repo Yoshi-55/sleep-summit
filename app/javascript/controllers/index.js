9369c3f8371556abfacd8ed4148d301cfe779bb9
Yoshi-55 <ysmr9855@gmail.com>
2025-10-13 18:48:42 +0900

    🔥 remove: 不要ファイル削除、/test削除とそれに伴うci.yml修正 - minitest不要のため削除 - hello_controller等のデフォルトファイルも不要なので削除

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,7 +1,5 @@
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
 import FlashController from "./flash_controller"
 
-application.register("hello", HelloController)
 application.register("flash", FlashController)
