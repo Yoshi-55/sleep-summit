4674f2cb473c24a46178e231a9b732c75a1aa856
Yoshi-55 <ysmr9855@gmail.com>
2025-10-15 13:38:18 +0900

    ♻️ refactor: userのFactoryBotファイルを作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe User, type: :model do
   describe "バリデーションチェック" do
     context "有効な値の場合" do
       it "バリデーションを通過すること" do
-        user = User.new(email: "test@example.com", password: "password")
+        user = FactoryBot.build(:user)
         expect(user).to be_valid
         expect(user.errors).to be_empty
       end
@@ -12,7 +12,7 @@ RSpec.describe User, type: :model do
 
     context "メールアドレスがない場合" do
       it "バリデーションに失敗すること" do
-        user = User.new(email: nil, password: "password")
+        user = FactoryBot.build(:user, email: nil)
         expect(user).not_to be_valid
         expect(user.errors[:email]).not_to be_empty
       end
@@ -20,7 +20,7 @@ RSpec.describe User, type: :model do
 
     context "パスワードがない場合" do
       it "バリデーションに失敗すること" do
-        user = User.new(email: "test@example.com", password: nil)
+        user = FactoryBot.build(:user, password: nil)
         expect(user).not_to be_valid
         expect(user.errors[:password]).not_to be_empty
       end
@@ -28,8 +28,8 @@ RSpec.describe User, type: :model do
 
     context "メールアドレスが重複している場合" do
       it "バリデーションに失敗すること" do
-        User.create(email: "test@example.com", password: "password")
-        user = User.new(email: "test@example.com", password: "password")
+        FactoryBot.create(:user)
+        user = FactoryBot.build(:user)
         expect(user).not_to be_valid
         expect(user.errors[:email]).not_to be_empty
       end
@@ -37,7 +37,7 @@ RSpec.describe User, type: :model do
 
     context "パスワードが短すぎる場合" do
       it "バリデーションに失敗すること" do
-        user = User.new(email: "test@example.com", password: "123")
+        user = FactoryBot.build(:user, password: "123")
         expect(user).not_to be_valid
         expect(user.errors[:password]).not_to be_empty
       end
@@ -46,7 +46,7 @@ RSpec.describe User, type: :model do
 
   describe "sleep_recordsとの関連" do
     it "ユーザー削除時にsleep_recordsも削除されること" do
-      user = User.create(email: "test@example.com", password: "password")
+      user = FactoryBot.create(:user)
       user.sleep_records.create(wake_time: Time.current, bed_time: Time.current + 8.hours)
       expect { user.destroy }.to change { SleepRecord.count }.by(-1)
     end
