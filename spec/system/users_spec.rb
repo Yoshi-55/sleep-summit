62e194f0599e1ea2302e3348d8c1773f02fe45e5
Yoshi-55 <ysmr9855@gmail.com>
2025-10-17 00:36:39 +0900

    👍update: システムテストi18n化 多言語対応時や単語変更時に変更が用意になるよう変更 ローカルではテスト通過

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -3,22 +3,22 @@ require 'rails_helper'
 RSpec.describe "Users", type: :system do
   it "ユーザー登録できること" do
     visit new_user_registration_path
-    fill_in "Name", with: "admin"
-    fill_in "Email", with: "test@example.com"
-    fill_in "Password", with: "password"
-    fill_in "Password confirmation", with: "password"
-    click_button "Sign up"
+    fill_in I18n.t('activerecord.attributes.user.name'), with: "admin"
+    fill_in I18n.t('activerecord.attributes.user.email'), with: "test@example.com"
+    fill_in I18n.t('activerecord.attributes.user.password'), with: "password"
+    fill_in I18n.t('activerecord.attributes.user.password_confirmation'), with: "password"
+    click_button I18n.t('devise.shared.links.sign_up')
 
-    expect(page).to have_content("Welcome! You have signed up successfully.")
+    expect(page).to have_content(I18n.t('devise.registrations.signed_up'))
   end
 
   it "ログインできること" do
     FactoryBot.create(:user)
     visit new_user_session_path
-    fill_in "Email", with: "test@example.com"
-    fill_in "Password", with: "password"
-    click_button "Log in"
+    fill_in I18n.t('activerecord.attributes.user.email'), with: "test@example.com"
+    fill_in I18n.t('activerecord.attributes.user.password'), with: "password"
+    click_button I18n.t('devise.shared.links.sign_in')
 
-    expect(page).to have_content("Signed in successfully.")
+    expect(page).to have_content(I18n.t('devise.sessions.signed_in'))
   end
 end
