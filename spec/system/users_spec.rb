7704edd918637ce1aa05bea94ae66a3740c64c79
Yoshi-55 <ysmr9855@gmail.com>
2025-10-19 01:27:58 +0900

    👍 update: BDDベースのテスト作成 issueにてG/W/Tをまとめてシステムテストに落とし込みました #19

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :system do
-  it "ユーザー登録できること" do
+  scenario "ユーザー登録できること" do
     visit new_user_registration_path
     fill_in I18n.t('activerecord.attributes.user.name'), with: "admin"
     fill_in I18n.t('activerecord.attributes.user.email'), with: "test@example.com"
@@ -12,17 +12,23 @@ RSpec.describe "Users", type: :system do
     expect(page).to have_content(I18n.t('devise.registrations.signed_up'))
   end
 
-  it "ログインできること" do
-    FactoryBot.create(:user)
-    visit new_user_session_path
-    fill_in I18n.t('activerecord.attributes.user.email'), with: "test@example.com"
-    fill_in I18n.t('activerecord.attributes.user.password'), with: "password"
-    click_button I18n.t('devise.shared.links.sign_in')
+  feature "sign in" do
+    given(:user) { FactoryBot.create(:user) }
 
-    expect(page).to have_content(I18n.t('devise.sessions.signed_in'))
+    scenario "ログインできること" do
+      visit new_user_session_path
+      fill_in I18n.t('activerecord.attributes.user.email'), with: user.email
+      fill_in I18n.t('activerecord.attributes.user.password'), with: user.password
+      click_button I18n.t('devise.shared.links.sign_in')
+
+      expect(page).to have_content(I18n.t('devise.sessions.signed_in'))
+
+      visit profile_path
+
+      expect(page).to have_current_path(profile_path)
+      expect(page).to have_content(I18n.t('profiles.show.page_title'))
+    end
   end
-
-  # ------------------------------------------------
 
   describe "Profile" do
     let(:user) { FactoryBot.create(:user) }
@@ -35,20 +41,26 @@ RSpec.describe "Users", type: :system do
       visit profile_path
     end
 
-    it "自分のプロフィールページが表示できる" do
+    scenario "自分のプロフィールページが表示できる" do
       expect(page).to have_content(user.name)
     end
 
-    it "メールアドレスが表示されないこと" do
+    scenario "メールアドレスが表示されないこと" do
       expect(page).not_to have_content(user.email)
     end
 
-    it "ページタイトルがI18nで表示される" do
+    scenario "ページタイトルがI18nで表示される" do
       expect(page).to have_content(I18n.t('profiles.show.page_title'))
     end
   end
 
-  # ------------------------------------------------
+  describe "Profile Edit - Access control" do
+    scenario "未ログインで編集ページにアクセスするとログイン画面へリダイレクト" do
+      visit edit_profile_path
+      expect(page).to have_current_path(new_user_session_path)
+      expect(page).to have_content(I18n.t('devise.failure.unauthenticated'))
+    end
+  end
 
   describe "Profile Edit" do
     let(:user) { FactoryBot.create(:user) }
@@ -61,28 +73,43 @@ RSpec.describe "Users", type: :system do
       visit edit_profile_path
     end
 
-    it "プロフィール編集ページが表示できること" do
-      expect(page).to have_content(I18n.t('profiles.edit.page_title'))
-    end
+    context "ログイン済みユーザー" do
+      scenario "編集フォームと現在のプロフィール情報が表示される" do
+        expect(page).to have_content(I18n.t('profiles.edit.page_title'))
 
-    it "名前が空だとエラーになること" do
-      fill_in I18n.t('activerecord.attributes.user.name'), with: ""
-      click_button I18n.t('profiles.edit.submit')
-      expect(page).to have_content(I18n.t('errors.messages.blank'))
-    end
+        expect(page).to have_field(I18n.t('profiles.edit.name_label'), with: user.name)
+      end
 
-    it "名前を変更して保存できること" do
-      fill_in I18n.t('activerecord.attributes.user.name'), with: "newname"
-      click_button I18n.t('profiles.edit.submit')
-      expect(page).to have_content(I18n.t('profiles.update.success'))
-      expect(page).to have_content("newname")
-    end
+      scenario "有効な名前を入力して保存すると成功メッセージと新しい名前が表示される" do
+        fill_in I18n.t('profiles.edit.name_label'), with: "新しい名前"
+        click_button I18n.t('profiles.edit.submit')
 
-    it "プロフィールアイコンを変更して保存できること" do
-      attach_file I18n.t('activerecord.attributes.user.avatar'), Rails.root.join('spec/fixtures/files/sample_avatar.png')
-      click_button I18n.t('profiles.edit.submit')
-      expect(page).to have_content(I18n.t('profiles.update.success'))
-      expect(page).to have_selector("img[src$='sample_avatar.png']")
+        expect(page).to have_content(I18n.t('profiles.update.success'))
+        expect(page).to have_content("新しい名前")
+      end
+
+      scenario "名前を空欄で保存するとエラーメッセージが表示される" do
+        fill_in I18n.t('profiles.edit.name_label'), with: ""
+        click_button I18n.t('profiles.edit.submit')
+
+        expect(page).to have_content(I18n.t('errors.messages.blank'))
+      end
+
+      scenario "png画像を選択して保存すると成功メッセージと新しいアイコンが表示される" do
+        attach_file I18n.t('profiles.edit.avatar_label'), Rails.root.join('spec/fixtures/files/sample_avatar.png')
+        click_button I18n.t('profiles.edit.submit')
+
+        expect(page).to have_content(I18n.t('profiles.update.success'))
+
+        expect(page).to have_selector("img[alt='プロフィールアイコン']")
+      end
+
+      scenario "許可されていない拡張子(gif)を選択するとバリデーションエラーが表示される" do
+        attach_file I18n.t('profiles.edit.avatar_label'), Rails.root.join('spec/fixtures/files/invalid_avatar.gif')
+        click_button I18n.t('profiles.edit.submit')
+
+        expect(page).to have_content(I18n.t('errors.messages.invalid_avatar_type', default: I18n.t('errors.messages.invalid')))
+      end
     end
   end
 end
