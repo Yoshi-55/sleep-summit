351fdd40ec8d275305a0957207768b620521fc5b
Yoshi-55 <ysmr9855@gmail.com>
2025-10-19 15:22:29 +0900

    ✨ feat: プロフィール編集・更新処理を追加 ユーザーのプロフィール編集・更新処理（name, avatar）をコントローラに実装 バリデーションエラー時は編集画面へリダイレクトして、フラッシュでエラーメッセージを表示します

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,19 @@ class UsersController < ApplicationController
   def edit
     @user = current_user
   end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to profile_path, notice: I18n.t("profiles.update.success")
+    else
+      redirect_to edit_profile_path, alert: I18n.t("profiles.update.error")
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :avatar)
+  end
 end
